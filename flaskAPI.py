4e0ab739ab81e074e998bca741417d311f5ebc43
johncanthony <john.claud.anthony@gmail.com>
2017-03-12 16:48:35 -0700

    flaskAPI.py - Updated listing method to allow for zone specification               Added the zone specific api path with a GET request

--- flaskAPI.py
+++ flaskAPI.py
@@ -11,14 +11,32 @@ def list_instances(compute, project, zone):
 
 app = Flask(__name__)
 
-@app.route('/api/instances',methods=['GET'])
-def GET_instances():
+#Take the URL supplied by the GCP API for zone and extract the 
+def zone_explode(zoneURL):
+
+	if(zoneURL==None):
+		return ""
+
+	splitURL=zoneURL.split('/')
+	
+	return splitURL[len(splitURL)-1]
+
+
+#Build Instances JSON
+#If no zone is specified then all zones will be investigated
+#If zone is supplied then only the instances in that zone will be returned
+def build_instances_json(zone=None):
 
 	#VARS
 	JSON={}
 	INSTANCES=[]
 	itemCount=0
+	ZONES=[zone]
+	
+	print(ZONES)
 
+	if(ZONES==[None]):
+		ZONES=GLOBALS.ZONES
 
 	#Obtain Credentials and Scope
 	credentials = GoogleCredentials.get_application_default()
@@ -27,24 +45,41 @@ def GET_instances():
 	compute = discovery.build('compute', 'v1', credentials=credentials)
 
 	#Read through all zones to pull the instances in the zone
-	for zone in GLOBALS.ZONES:
+	for zone in ZONES:
 		instances = list_instances(compute,GLOBALS.PROJECT,zone)	
 		if(instances == None):
 			continue
 
+		
 		for instance in instances:
-		    INSTANCES.append({itemCount:str(instance['name'])})
+	            zone=zone_explode(str(instance['zone']))
+			
+		    if( "quick-instance" not in instance['name']):
+			continue
+
+		    INSTANCES.append({"name":str(instance['name']),"zone":zone})
 		    itemCount+=1
 		
 
-	JSON['max_index']=itemCount-1
+	JSON['object_count']=itemCount
 	JSON['instances']=INSTANCES
 
-	returned_json = jsonify(JSON)
+	return jsonify(JSON)
+
 
 	
-	
-	return returned_json
+
+@app.route('/api/instances',methods=['GET'])
+def GET_instances():
+
+	return build_instances_json()
+
+
+@app.route('/api/instances/<string:zone>', methods=['GET'])
+def get_instances_zone(zone):
+
+	#TODO - ADD check for actual zone and throw error page if wrong	
+	return build_instances_json(zone)
 
 if __name__ == '__main__':
 	app.run(host='127.0.0.1', port=8080, debug=True)
