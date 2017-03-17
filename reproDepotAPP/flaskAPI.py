f61af3d1a70e93088edc0a2c4648fc55c0bf2cee
johncanthony <john.claud.anthony@gmail.com>
2017-03-17 01:54:45 -0700

    Added instance creation to the AppEngine directory

--- reproDepotAPP/flaskAPI.py
+++ reproDepotAPP/flaskAPI.py
@@ -1,8 +1,11 @@
-from flask import Flask, jsonify
+from flask import Flask,jsonify
+from flask.ext.api import status
 from oauth2client.client import GoogleCredentials
 from googleapiclient import discovery
 import GLOBALS
+import time
 
+#Use googleapiclient to pull list of compute instances from zone
 def list_instances(compute, project, zone):
 
 	result = compute.instances().list(project=project, zone=zone).execute()
@@ -10,6 +13,12 @@ def list_instances(compute, project, zone):
 
 
 app = Flask(__name__)
+
+
+#Check Zone for Validity
+def check_zone(zone):
+	return(zone in GLOBALS.ZONES)
+
 
 #Take the URL supplied by the GCP API for zone and extract the 
 def zone_explode(zoneURL):
@@ -36,7 +45,7 @@ def get_authScope(resource):
 #Build Instances JSON
 #If no zone is specified then all zones will be investigated
 #If zone is supplied then only the instances in that zone will be returned
-def build_instances_json(compute, zone=None):
+def build_instances_json(compute,project, zone=None):
 
 	#VARS
 	JSON={}
@@ -48,11 +57,14 @@ def build_instances_json(compute, zone=None):
 
 	if(ZONES==[None]):
 		ZONES=GLOBALS.ZONES
+	else:	
+		if(not check_zone(zone)):
+			return "Zone Not Found", status.HTTP_404_NOT_FOUND
 
 	
 	#Read through all zones to pull the instances in the zone
 	for zone in ZONES:
-		instances = list_instances(compute,GLOBALS.PROJECT,zone)	
+		instances = list_instances(compute,project,zone)	
 		
 		if(instances == None):
 			continue
@@ -66,7 +78,7 @@ def build_instances_json(compute, zone=None):
 
 			INSTANCES.append({"name":str(instance['name']),"zone":zone})
 			itemCount+=1
-
+		
 
 	JSON['object_count']=itemCount
 	JSON['instances']=INSTANCES
@@ -74,37 +86,129 @@ def build_instances_json(compute, zone=None):
 	return jsonify(JSON)
 
 
+#Create Quick Instance
+def create_instance(compute,zone="us-central1-c"):
+
+    	#Genereate Name
+	uid=int(time.time()*1000)
+	name="quick-instance"+str(uid)
+	print("Name:"+str(name))
+
+	# Get the latest Debian Jessie image.
+	image_response = compute.images().getFromFamily(project='debian-cloud', family='debian-8').execute()
+	source_disk_image = image_response['selfLink']
+
+	# Configure the machine
+	machine_type = "zones/%s/machineTypes/n1-standard-1" % zone
+
+	config = {
+		'name': name,
+		'machineType': machine_type,
+
+		# Specify the boot disk and the image to use as a source.
+		'disks': [
+			{
+				'boot': True,
+				'autoDelete': True,
+				'initializeParams': {
+					'sourceImage': source_disk_image,
+				}
+			}
+		],
+
+		# Specify a network interface with NAT to access the public
+		# internet.
+		'networkInterfaces': [{
+			'network': 'global/networks/default',
+			'accessConfigs': [
+				{'type': 'ONE_TO_ONE_NAT', 'name': 'External NAT'}
+			]
+		}],
+
+		# Allow the instance to access cloud storage and logging.
+		'serviceAccounts': [{
+			'email': 'default',
+			'scopes': [
+				'https://www.googleapis.com/auth/devstorage.read_write',
+				'https://www.googleapis.com/auth/logging.write'
+			]
+		}]
+
+
+	}
+
+	response=compute.instances().insert(project=GLOBALS.PROJECT,zone=zone,body=config).execute()
+
+	return response['status']
+
 #Delete Instance in zone
 def delete_instance(compute, zone, name):
+	
 	project=GLOBALS.PROJECT
+
 	return compute.instances().delete(project=project,zone=zone,instance=name).execute()
 
+	
 
-
-@app.route('/api/list/instances',methods=['GET'])
+@app.route('/api/instances',methods=['GET'])
 def GET_instances():
 	
 	compute=get_authScope('compute')
-	return build_instances_json(compute)
+	return build_instances_json(compute,GLOBALS.PROJECT)
 
 
-@app.route('/api/list/instances/<string:zone>', methods=['GET'])
+
+
+@app.route('/api/instances/<string:zone>', methods=['GET'])
 def GET_instances_zone(zone):
 
 	#TODO - ADD check for actual zone and throw 404 error if wrong	
 	compute=get_authScope('compute')
-	return build_instances_json(compute,zone)
 
-@app.route('/api/delete/instances/<string:zone>/<string:name>', methods=['DELETE'])
+	return build_instances_json(compute,GLOBALS.PROJECT,zone)
+
+
+
+@app.route('/api/instances/<string:zone>/<string:name>', methods=['DELETE'])
 def DELETE_inst(zone,name):
 	compute=get_authScope('compute')
-	#if instance exists return 200 with success
-	#else thrown 404
-	status = delete_instance(compute,zone,name)
+	
+	#Check Zone Validity
+	if(not check_zone(zone)):
+		return "Zone Not Found", status.HTTP_404_NOT_FOUND
+	
 
-	print(status.keys())
+	return delete_instance(compute,zone,name)
 
-	return "Success"
+
+
+@app.route('/api/instances/<string:zone>',methods=['POST'])
+def POST_create_instances_zone(zone="us-central1-c"):
+
+	compute=get_authScope('compute')
+	print(zone)
+	#Check Zone Validity
+	if(not check_zone(zone)):
+		return "Zone Not Found", status.HTTP_404_NOT_FOUND
+
+	create_instance(compute,zone)	
+
+	return "Created"
+
+@app.route('/api/instances',methods=['POST'])
+def POST_create_instances():
+
+	zone="us-central1-c"
+	compute=get_authScope('compute')
+	#Check Zone Validity
+	if(not check_zone(zone)):
+		return "Zone Not Found", status.HTTP_404_NOT_FOUND
+
+	x=create_instance(compute)	
+	print(x)
+	
+	return "Created"
+
 	
 if __name__ == '__main__':
 	app.run(host='127.0.0.1', port=8080, debug=True)
